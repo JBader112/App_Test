c39410e22f3abb9090e9d248d95305716ff3ceb6
Justin-DevMac <jbader112@gmail.com>
2018-05-24 19:05:29 -0700

    Initial Commit
    Initial Xcode Additions

---
+++ Test_App/Test_App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_App
+//
+//  Created by Dev on 5/24/18.
+//  Copyright © 2018 Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test_App/Test_App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test_App
+//
+//  Created by Dev on 5/24/18.
+//  Copyright © 2018 Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
